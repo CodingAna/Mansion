ca83ff4c73c9aaf55968d014b46f24215686f64a
Lukas Baginski <74157379+LukasBaginski@users.noreply.github.com>
2021-11-10 09:31:23 +0100

    Added mac version

---
+++ CopyAppMac.sh
@@ -0,0 +1,11 @@
+title CopyApp
+clear
+
+rm -f /Volumes/Untitled/@MainMem/PROGRAM/MANSION.g3m
+rm -f /Volumes/Untitled/@MainMem/PROGRAM/MANSION.txt
+
+cp MANSION.txt /Volumes/Untitled/@MainMem/PROGRAM/
+
+clear
+echo "Copied application successfully."
+exit 0
